f32ef52314e4f358eb66855bba7da44b2463c68c
Duynd03 <duyanguyen1811@gmail.com>
2024-11-24 23:27:38 +0700

    Update and destroy Monhoc

--- app/Http/Controllers/MonHocController.php
+++ app/Http/Controllers/MonHocController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers;
 use App\Models\MonHoc;
 use Illuminate\Http\Request;
 use App\Models\KhoaDaoTao;
+use Flasher\Toastr\Laravel\Facade\Toastr;
 class MonHocController extends Controller
 {
     public function index(Request $request)
@@ -17,7 +18,68 @@ class MonHocController extends Controller
     }
     public function store(Request $request)
     {
-        dd(request());
+        try {
+            
+            $check = MonHoc::where('ma_mon_hoc', $request->ma_mon_hoc)->first();
+            if ($check) {
+                Toastr::error('Mã môn học đã tồn tại', 'Lỗi');
+                return redirect()->route('mon-hoc.index');
+            }
+
+            $mon_hoc_moi = [
+                'ma_mon_hoc' => $request->ma_mon_hoc,
+                'ten_mon_hoc' => $request->ten_mon_hoc,
+                'so_tin_chi' => $request->so_tin_chi,
+                'ma_khoa_dao_tao_id' => $request->ma_khoa_dao_tao_id
+            ];
+            $mon_hoc = MonHoc::create($mon_hoc_moi);
+            if ($mon_hoc) {
+                Toastr::success('Thêm mới môn học thành công', 'Thành công');
+            } else {
+                Toastr::error('Thêm mới môn học tạo thất bại', 'Lỗi');
+            }
+            return redirect()->route('mon-hoc.index');
+        } catch (\Exception $e) {
+           
+            Toastr::error('Có lỗi xảy ra, vui lòng thử lại', 'Lỗi');
+            return redirect()->route('mon-hoc.index');
+        }
+    }
+    public function update($mon_hoc, Request $request){
+        try{
+            $mon_hoc = MonHoc::find($mon_hoc);
+            if($mon_hoc){
+                $mon_hoc->update([
+                    'ma_mon_hoc' => $request->ma_mon_hoc ?? $mon_hoc->ma_mon_hoc,
+                    'ten_mon_hoc' => $request->ten_mon_hoc ?? $mon_hoc->ten_mon_hoc,
+                    'so_tin_chi' => $request->so_tin_chi ?? $mon_hoc->so_tin_chi,
+                    'ma_khoa_dao_tao_id' => $request->ma_khoa_dao_tao_id ?? $mon_hoc->ma_khoa_dao_tao_id
+                ]);
+                Toastr::success('Cập nhật khoa đào tạo thành công', 'Thành công');
+            }else{
+                Toastr::error('Cập nhật khoa đào tạo thất bại', 'Lỗi');
+            }
+            return redirect()->route('mon-hoc.index');
+
+        } catch (\Exception $e) {
+            Toastr::error('Có lỗi xảy ra, vui lòng thử lại', 'Lỗi');
+            return redirect()->route('mon-hoc.index');
+        }
+    }
+    public function destroy($mon_hoc){
+        try{
+            $mon_hoc = MonHoc::find($mon_hoc);
+            if($mon_hoc){
+                $mon_hoc->delete();
+                Toastr::success('Xóa môn học thành công', 'Thành công');
+            }else{
+                Toastr::error('Xóa môn học thất bại', 'Lỗi');
+            }
+            return redirect()->route('mon-hoc.index');
+        } catch (\Exception $e) {
+            Toastr::error('Có lỗi xảy ra, vui lòng thử lại', 'Lỗi');
+            return redirect()->route('mon-hoc.index');
+        }
     }
     
 }
--- resources/views/monHoc/list.blade.php
+++ resources/views/monHoc/list.blade.php
@@ -71,8 +71,15 @@
                                             <td>{{ $mon_hoc->so_tin_chi }}</td>
                                             <td>{{$mon_hoc->khoaDaoTao->ten_khoa_dao_tao}}</td>
                                             <td>
-                                                <a href="" class="btn btn-sm btn-primary" role="button">Edit</a>
-                                                <a href="" class="btn btn-sm btn-danger" role="button">Delete</a>
+                                            @php
+                                                    $data_object = json_encode($mon_hoc);
+                                                @endphp
+                                                <a href="#" data-object="{{ $data_object }}" class="btn btn-sm btn-primary edit-button" 
+                                                role="button">Chỉnh sửa</a>
+                                            
+                                                <a href="#" data-id="{{ $mon_hoc->id }}"
+                                                    data-name="{{ $mon_hoc->ten_mon_hoc }}"
+                                                    class="btn btn-sm btn-danger delete-btn" role="button">Xoá</a>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -122,7 +129,7 @@
                         </div>
                         <div class="form-group">
                             <label class="form-label" for="exampleFormControlSelect1">Khoa đào tạo</label>
-                            <select class="form-select" id="exampleFormControlSelect1">
+                            <select class="form-select" id="exampleFormControlSelect1" name="ma_khoa_dao_tao_id">
                             <option selected="" disabled="">Chọn khoa đào tạo</option>
                             @foreach($khoa_dao_taos  as $khoa_dao_tao)
                             <option value="{{ $khoa_dao_tao->id }}">{{ $khoa_dao_tao->ten_khoa_dao_tao }}</option>
@@ -139,17 +146,120 @@
         </div>
     </div>
 
+
+
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Chỉnh sửa môn học</h5>
+                    <button type="button" class="close close-modal" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('mon-hoc.update', ['mon_hoc' => 0]) }}" method="POST" >
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="ma_mon-hoc" class="form-label">Mã môn học</label>
+                            <input type="text" class="form-control" id="ma_mon_hoc" name="ma_mon_hoc">
+                        </div>
+                        <div class="mb-3">
+                            <label for="ten_mon_hoc" class="form-label">Tên môn học</label>
+                            <input type="text" class="form-control" id="ten_mon_hoc"
+                                name="ten_mon_hoc">
+                        </div>
+                        <div class="mb-3">
+                            <label for="so_tin_chi" class="form-label">Số tín chỉ</label>
+                            <input type="text" class="form-control" id="so_tin_chi" name="so_tin_chi">
+                        </div>
+                        <div class="form-group">
+                            <label class="form-label" for="exampleFormControlSelect1">Khoa đào tạo</label>
+                            <select class="form-select" id="exampleFormControlSelect1"  name="ma_khoa_dao_tao_id">
+                                <option selected="" disabled="">Chọn khoa đào tạo</option>
+                                @foreach($khoa_dao_taos  as $khoa_dao_tao)
+                                    <option value="{{ $khoa_dao_tao->id }}">{{ $khoa_dao_tao->ten_khoa_dao_tao }}</option>
+                                @endforeach       
+                            </select>
+                        </div>      
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary close-modal" data-dismiss="modal">Huỷ
+                        bỏ</button>
+                    <button type="button" class="btn btn-primary" id="submit-update-btn">Chỉnh sửa</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">Xoá môn học</h5>
+                    <button type="button" class="close close-modal" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    <form action="{{ route('mon-hoc.destroy', ['mon_hoc' => 0]) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <h3 id="delete_title">Xác nhận xoá môn học: </h3>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary close-modal" data-dismiss="modal">Huỷ</button>
+                    <button type="button" id="delete-submit-btn" class="btn btn-primary">Xoá</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
+         var update_id = 0;
         $('#create-new-btn').on('click', function() {
             $('#createNewModal').modal('show');
         });
+        $('.edit-button').on('click', function() {
+            data = $(this).data('object');
+            update_id = data.id;
+            $('#editModal form').attr('action', '/mon-hoc/' + update_id);
+            $('#editModal #ma_mon_hoc').val(data.ma_mon_hoc);
+            $('#editModal #ten_mon_hoc').val(data.ten_mon_hoc);
+            $('#editModal #so_tin_chi').val(data.so_tin_chi);
+            $('#editModal #ma_khoa_dao_tao_id').val(data.ma_khoa_dao_tao_id);            
+            $('#exampleFormControlSelect1 option[value="' + data.ma_khoa_dao_tao_id + '"]').prop('selected', true);
+            $('#editModal').modal('show');
+        });
+        $('#submit-update-btn').on('click', function() {
+            $('#editModal form').submit();
+        });
 
         $('#submit-btn').on('click', function() {
+            console.log('Button clicked');
             $('#form-create').submit();
+        });
+        $('.delete-btn').on('click', function() {
+            delete_id = $(this).data('id');
+            $('#delete_title').text('Xác nhận xoá môn học: ' + $(this).data('name'));
+            $('#deleteModal form').attr('action', '/mon-hoc/' + delete_id);
+            $('#deleteModal').modal('show');
+        });
+
+        $('#delete-submit-btn').on('click', function() {
+            $('#deleteModal form').submit();
         });
 
         $('.close-modal').on('click', function() {
             $('#createNewModal').modal('hide');
+            $('#editModal').modal('hide');
+            $('#deleteModal').modal('hide');
         });
     </script>
 </x-app-layout>
