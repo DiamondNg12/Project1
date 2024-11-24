416af470a3ddf2d4b8621a31f8d961d6fef838d6
DiamondN <diamond12.ng@gmail.com>
2024-11-24 14:38:34 +0700

    update function action KhoaDaoTao

--- app/Models/KhoaDaoTao.php
+++ app/Models/KhoaDaoTao.php
@@ -8,4 +8,13 @@ use Illuminate\Database\Eloquent\Model;
 class KhoaDaoTao extends Model
 {
     use HasFactory;
+
+    protected $table = 'khoa_dao_taos';
+
+    protected $fillable = [
+        'ma_khoa_dao_tao',
+        'ten_khoa_dao_tao',
+        'ngay_thanh_lap',
+        'tinh_trang'
+    ];
 }
--- resources/views/khoaDaoTao/list.blade.php
+++ resources/views/khoaDaoTao/list.blade.php
@@ -46,8 +46,9 @@
                             <h5 class="card-title">Danh sách khoa đào tạo</h5>
                         </div>
                         <div class="card-action">
-                            <a href="#" class="btn btn-sm btn-primary" id="create-new-btn" role="button">Thêm Khoa Đào tạo</a>
-                       </div>
+                            <a href="#" class="btn btn-sm btn-primary" id="create-new-btn" role="button">Thêm
+                                Khoa Đào tạo</a>
+                        </div>
                     </div>
                     <div class="card-body px-0">
                         <div class="table-responsive ">
@@ -71,7 +72,8 @@
                                             <td>{{ $key++ }}</td>
                                             <td>{{ $khoa_dao_tao->ma_khoa_dao_tao }}</td>
                                             <td>{{ $khoa_dao_tao->ten_khoa_dao_tao }}</td>
-                                            <td>{{ \Carbon\Carbon::parse($khoa_dao_tao->ngay_thanh_lap)->format('d/m/Y') }}</td>
+                                            <td>{{ \Carbon\Carbon::parse($khoa_dao_tao->ngay_thanh_lap)->format('d/m/Y') }}
+                                            </td>
                                             <td>
                                                 @if ($khoa_dao_tao->tinh_trang == 1)
                                                     <span class="badge bg-success">Đang hoạt động</span>
@@ -80,8 +82,15 @@
                                                 @endif
                                             </td>
                                             <td>
-                                                <a href="#" class="btn btn-sm btn-primary" role="button">Edit</a>
-                                                <a href="#" class="btn btn-sm btn-danger" role="button">Delete</a>
+                                                @php
+                                                    $data_object = json_encode($khoa_dao_tao);
+                                                @endphp
+                                                <a href="#" data-object="{{ $data_object }}"
+                                                    class="btn btn-sm btn-primary edit-button" role="button">Chỉnh
+                                                    sửa</a>
+                                                <a href="#" data-id="{{ $khoa_dao_tao->id }}"
+                                                    data-name="{{ $khoa_dao_tao->ten_khoa_dao_tao }}"
+                                                    class="btn btn-sm btn-danger delete-btn" role="button">Xoá</a>
                                             </td>
                                         </tr>
                                     @endforeach
@@ -124,14 +133,81 @@
                     </form>
                 </div>
                 <div class="modal-footer">
-                    <button type="button" class="btn btn-secondary close-modal" data-dismiss="modal">Huỷ bỏ</button>
+                    <button type="button" class="btn btn-secondary close-modal" data-dismiss="modal">Huỷ
+                        bỏ</button>
                     <button type="button" class="btn btn-primary" id="submit-btn">Thêm mới</button>
                 </div>
             </div>
         </div>
     </div>
 
+    <div class="modal fade" id="editModal" tabindex="-1" role="dialog" aria-labelledby="editModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="editModalLabel">Chỉnh sửa Khoa đào tạo</h5>
+                    <button type="button" class="close close-modal" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+                    <form action="{{ route('khoa-dao-tao.update', ['khoa_dao_tao' => 0]) }}" method="POST" id="form-create">
+                        @csrf
+                        @method('PUT')
+                        <div class="mb-3">
+                            <label for="ma_khoa_dao_tao" class="form-label">Mã khoa đào tạo</label>
+                            <input type="text" class="form-control" id="ma_khoa_dao_tao" name="ma_khoa_dao_tao">
+                        </div>
+                        <div class="mb-3">
+                            <label for="ten_khoa_dao_tao" class="form-label">Tên khoa đào tạo</label>
+                            <input type="text" class="form-control" id="ten_khoa_dao_tao"
+                                name="ten_khoa_dao_tao">
+                        </div>
+                        <div class="mb-3">
+                            <label for="ngay_thanh_lap" class="form-label">Ngày thành lập</label>
+                            <input type="date" class="form-control" id="ngay_thanh_lap" name="ngay_thanh_lap">
+                        </div>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary close-modal" data-dismiss="modal">Huỷ
+                        bỏ</button>
+                    <button type="button" class="btn btn-primary" id="submit-update-btn">Chỉnh sửa</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="modal fade" id="deleteModal" tabindex="-1" role="dialog" aria-labelledby="deleteModalLabel"
+        aria-hidden="true">
+        <div class="modal-dialog" role="document">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="deleteModalLabel">Xoá Khoa đào tạo</h5>
+                    <button type="button" class="close close-modal" data-dismiss="modal" aria-label="Close">
+                        <span aria-hidden="true">&times;</span>
+                    </button>
+                </div>
+                <div class="modal-body">
+
+                    <form action="{{ route('khoa-dao-tao.destroy', ['khoa_dao_tao' => 0]) }}" method="post">
+                        @csrf
+                        @method('DELETE')
+                        <h3 id="delete_title">Xác nhận xoá khoa đào tạo: </h3>
+                    </form>
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-secondary close-modal" data-dismiss="modal">Huỷ</button>
+                    <button type="button" id="delete-submit-btn" class="btn btn-primary">Xoá</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
     <script>
+        var update_id = 0;
+        var delete_id = 0;
         $('#create-new-btn').on('click', function() {
             $('#createNewModal').modal('show');
         });
@@ -140,8 +216,36 @@
             $('#form-create').submit();
         });
 
+        $('.edit-button').on('click', function() {
+            data = $(this).data('object');
+            update_id = data.id;
+            $('#editModal form').attr('action', '/khoa-dao-tao/' + update_id);
+            $('#editModal #ma_khoa_dao_tao').val(data.ma_khoa_dao_tao);
+            $('#editModal #ten_khoa_dao_tao').val(data.ten_khoa_dao_tao);
+            $('#editModal #ngay_thanh_lap').val(data.ngay_thanh_lap);
+            $('#editModal').modal('show');
+        });
+
+        $('#submit-update-btn').on('click', function() {
+            $('#editModal form').submit();
+        });
+
+
+        $('.delete-btn').on('click', function() {
+            delete_id = $(this).data('id');
+            $('#delete_title').text('Xác nhận xoá khoa đào tạo: ' + $(this).data('name'));
+            $('#deleteModal form').attr('action', '/khoa-dao-tao/' + delete_id);
+            $('#deleteModal').modal('show');
+        });
+
+        $('#delete-submit-btn').on('click', function() {
+            $('#deleteModal form').submit();
+        });
+
         $('.close-modal').on('click', function() {
             $('#createNewModal').modal('hide');
+            $('#editModal').modal('hide');
+            $('#deleteModal').modal('hide');
         });
     </script>
 
