f45186edc3f2e2902431b1715f2c24f840716a70
DiamondN <diamond12.ng@gmail.com>
2024-11-24 13:27:30 +0700

    publish MonHoc, KhoaHoc

--- resources/views/partials/dashboard/vertical-nav.blade.php
+++ resources/views/partials/dashboard/vertical-nav.blade.php
@@ -927,6 +927,36 @@
                         <span class="item-name">Khoa Đào Tạo</span>
                     </a>
                 </li>
+                <li class=" nav-item">
+                    <a class="nav-link {{ activeRoute(route('khoa-hoc.index')) }}"
+                        href="{{ route('khoa-hoc.index') }}">
+                        <i class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24"
+                                fill="currentColor">
+                                <g>
+                                    <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                </g>
+                            </svg>
+                        </i>
+                        <i class="sidenav-mini-icon"> K </i>
+                        <span class="item-name">Khoá học</span>
+                    </a>
+                </li>
+                <li class=" nav-item">
+                    <a class="nav-link {{ activeRoute(route('mon-hoc.index')) }}"
+                        href="{{ route('mon-hoc.index') }}">
+                        <i class="icon">
+                            <svg xmlns="http://www.w3.org/2000/svg" width="10" viewBox="0 0 24 24"
+                                fill="currentColor">
+                                <g>
+                                    <circle cx="12" cy="12" r="8" fill="currentColor"></circle>
+                                </g>
+                            </svg>
+                        </i>
+                        <i class="sidenav-mini-icon"> M </i>
+                        <span class="item-name">Môn học</span>
+                    </a>
+                </li>
                 {{-- <li class="nav-item">
                     <a class="nav-link  {{ activeRoute(route('special-pages.calender')) }}"
                         href="{{ route('special-pages.calender') }}">
